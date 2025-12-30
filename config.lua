0ef91ea9c3d64391bdc37f7f24ef4334ccad0fac
Laugh <70723697+xLaugh@users.noreply.github.com>
2025-12-30 01:11:05 +0100

    update

--- config.lua
+++ config.lua
@@ -6,6 +6,9 @@ Config.Locale = 'fr'
 Config.BossLocations = {
     ["police"] = vec3(-461.803619, 7117.808594, 22.383690),
     ["ems"] = vec3(-546.751953, 7393.100098, 12.838461),
+	--["burgershot"] = vec3(0.1, 0.2, 0.3),
+	--["unicorn"] = vec3(13, 22, 42),
+	--["weazel"] = vec3(19, 30, 2.45,
 }
 
 Config.JobColors = {
